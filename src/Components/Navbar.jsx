cd24ca68bb2de3d3f2d31c0631a6dc68745fe9d1
Shubh <jainshubh.200208@gmail.com>
2024-01-23 18:07:31 +0530

    fix : name change

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <div className="my-4 h-[60px] bg-white rounded-lg flex items-center justify-center gap-2 text-xl font-medium">
       <img src="/logos_firebase.png" alt="FireBase Image" />
-      <h1>FireBase Contact App</h1>
+      <h1>Contact Directory</h1>
     </div>
   );
 };
